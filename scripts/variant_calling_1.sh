d9cdf3cade301259b07169903567a759549a5c77
jcunha2023 <jcunha1@bu.edu>
2024-11-11 09:42:54 -0500

    Update variant_calling_1.sh

--- scripts/variant_calling_1.sh
+++ scripts/variant_calling_1.sh
@@ -24,6 +24,6 @@ rm $INPUT_SAM
 
 # # variant calling round 1
 gatk Mutect2 -R $REFERENCE -L $INTERVAL --mitochondria-mode -I ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam \
--O ${vcf_dir}${SAMPLE_ID}_variants_called_against_$REFTYPE_rCRS.vcf --min-base-quality-score 30
+-O ${vcf_dir}${SAMPLE_ID}_variants_called_against_${REFTYPE}_rCRS.vcf --min-base-quality-score 30
 rm ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam
 rm ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam.bai
