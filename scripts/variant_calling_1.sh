9e59451ffef8b1cf6d73f6ff5aabf866a2741f5c
jcunha2023 <jcunha1@bu.edu>
2024-11-10 20:30:59 -0500

    Update variant_calling_1.sh

--- scripts/variant_calling_1.sh
+++ scripts/variant_calling_1.sh
@@ -5,8 +5,8 @@ INPUT_SAM=$1
 REFERENCE=$2
 INTERVAL=$3
 
-temp_dir="tmp/"
-vcf_dir="vcf_files_rCRS/"
+temp_dir="/tmp/"
+vcf_dir="/vcf_files_rCRS/"
 
 # get sample id
 SAMPLE_ID_TEMP="${INPUT_SAM##*/}"
