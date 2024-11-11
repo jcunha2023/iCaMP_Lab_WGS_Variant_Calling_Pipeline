c44c28d77980b50dec3e7f149aa42653b68bd4e0
jcunha2023 <jcunha1@bu.edu>
2024-11-11 10:06:31 -0500

    Update variant_calling_1.sh

--- scripts/variant_calling_1.sh
+++ scripts/variant_calling_1.sh
@@ -6,8 +6,8 @@ REFERENCE=$2
 INTERVAL=$3
 REFTYPE=$4
 
-temp_dir="/tmp/"
-vcf_dir="/vcf_files_rCRS/"
+temp_dir="./tmp/"
+vcf_dir="./vcf_files_rCRS/"
 
 # get sample id
 SAMPLE_ID_TEMP="${INPUT_SAM##*/}"
