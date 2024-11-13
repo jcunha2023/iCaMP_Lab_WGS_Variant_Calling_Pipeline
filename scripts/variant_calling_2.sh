dc8ab4466bb719dfff4492c3616d1bab0e62dbc9
jcunha2023 <jcunha1@bu.edu>
2024-11-13 11:27:47 -0500

    Update variant call 2 script and relative paths in snakefile
    Modified variant calling script 2 to include a working directory parameter for the user to specify the working directory. Added additional code at the beginning to create directories needed to store output files.
    
    Also changed snakefile to use the WORKING_DIR variable for relative paths.

--- scripts/variant_calling_2.sh
+++ scripts/variant_calling_2.sh
@@ -14,11 +14,16 @@ SAMPLE_ID_TEMP="${CONSENSUS_REFERENCE##*/}"
 SAMPLE_ID="${SAMPLE_ID_TEMP%_consensus_ref.fa}"
 
 FQ_INPUT=$2
-SAM_OUTPUT="./sam_files_consensus/${SAMPLE_ID}_consensus.sam"
+WORKDIR=$3
 
-temp_dir="./tmp/"
-vcf_dir="./vcf_files_consensus/"
-consensus_dir="./consensus_reference_sequences/"
+## Make sam files directory and define sam file output
+mkdir -p "${WORKDIR}/sam_files_consensus/"
+SAM_OUTPUT="${WORKDIR}/sam_files_consensus/${SAMPLE_ID}_consensus.sam"
+
+## Define temporary and input/output directories for VCFs, consensus reference sequences, and intermediate files
+temp_dir="${WORKDIR}/tmp/"
+vcf_dir="${WORKDIR}/vcf_files_consensus/"
+consensus_dir="${WORKDIR}/consensus_reference_sequences/"
 
 ## create directory for the consensus reference
 gatk CreateSequenceDictionary -R ${CONSENSUS_REFERENCE}
@@ -43,16 +48,15 @@ bwa mem ${CONSENSUS_INDEX} ${FQ_INPUT} -K 100000000 -p -v 3 -Y > ${SAM_OUTPUT}
 ## preparing alignmed reads for variant calling
 gatk AddOrReplaceReadGroups -I ${SAM_OUTPUT} -O ${temp_dir}${SAMPLE_ID}-addedReadGroup.sam -LB Pond -PL ILLUMINA -PU 0 -SM ${SAMPLE_ID}
 samtools view -C -T ${CONSENSUS_REFERENCE} -o ${temp_dir}${SAMPLE_ID}-addedReadGroup.bam ${temp_dir}${SAMPLE_ID}-addedReadGroup.sam
-samtools sort ${temp_dir}${SAMPLE_ID}-addedReadGroup.bam -o ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam
-samtools index -b ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam
+samtools sort -@ 1 ${temp_dir}${SAMPLE_ID}-addedReadGroup.bam -o ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam
+samtools index -@ 1 -b ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam
 rm ${temp_dir}${SAMPLE_ID}-addedReadGroup.bam
 rm ${temp_dir}${SAMPLE_ID}-addedReadGroup.sam
-#rm ${SAM_OUTPUT}
-
+rm $INPUT_SAM
 
 ## call the genetic variants
 gatk Mutect2 -R ${CONSENSUS_REFERENCE} -L chrM \
 --mitochondria-mode -I ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam \
 -O ${vcf_dir}${SAMPLE_ID}_variants_called_against_consensus.vcf --min-base-quality-score 30
-#rm ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam
-#rm ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam.bai
+# rm ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam
+# rm ${temp_dir}${SAMPLE_ID}-addedReadGroup-sorted.bam.bai
